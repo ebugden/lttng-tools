745f1c58d3916f6d244e64123a2c5ac8f46e3470
Erica Bugden <ebugden@efficios.com>
2025-07-07 16:27:38 -0400

    tests: Check if signal used before adding handler (WIP)
    Recently, a long time was spent debugging a signal handler that was not
    being called before realizing that the handler registration was being
    overwritten by another function. Add check to reduce the chance of
    similar accidental overwriting happening.
    
    Bail out if this occurs since it indicates a logical coding error.
    
    There are other instances of handler registration to which this check
    was not added. The rationale for not adding the check to all instances
    is that only hacing a subset of the checks will still reduce the
    likelyhood of this kind of mistake.
    
    Change-Id: I1a200b691dc8efa8e48b2a2ff74b6112f05c204f
    Signed-off-by: Erica Bugden <ebugden@efficios.com>

--- tests/utils/utils.sh
+++ tests/utils/utils.sh
@@ -309,6 +309,19 @@ function get_pipe_max_size()
 	fi
 }
 
+# Assert that there is no handler currently registered for a signal.
+#  $1 = The signal name (e.g. SIGUSR1)
+#
+# Intended usage: Before registering a signal handler.
+function assert_no_handler_registered_for_signal()
+{
+	local signal_name=$1
+
+	if [ "$(trap -p "$signal_name")" != "" ]; then
+		LTTNG_BAIL_OUT "Signal ${signal_name} already has a registered handler. The testing code must be adjusted."
+	fi
+}
+
 # Return a space-separated string of online CPU IDs, based on
 # /sys/devices/system/cpu/online, or from 0 to nproc - 1 otherwise.
 function get_online_cpus()
@@ -955,6 +968,7 @@ function start_lttng_relayd_opt()
 	pid_file="$(mktemp -u)"
 	opts+=('--pid-file' "${pid_file}" "--sig-parent")
 	wait_until_ready=1
+	assert_no_handler_registered_for_signal SIGUSR1
 	trap 'wait_until_ready=0' SIGUSR1
 	# shellcheck disable=SC2086
 	if [[ -n "${log_file}" ]]; then
@@ -1170,6 +1184,7 @@ function start_lttng_sessiond_opt()
 	export LTTNG_SESSION_CONFIG_XSD_PATH
 
 	wait_until_ready=1
+	assert_no_handler_registered_for_signal SIGUSR1
 	trap 'wait_until_ready=0' SIGUSR1
 	if [ -n "$load_path" ]; then
 		diag "env $env_vars --load $load_path $consumerd ${opts[*]}"
